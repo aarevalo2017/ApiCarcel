400b52567b1c38abdc2128bb9fa24bf580eb5172
egatica56 <eduardoandresgaticaalvarez@gmail.com>
2017-10-24 21:51:59 -0300

    creacion de clases y creacion de DBContext. aun no se agrega al webconfig

---
+++ CarcelAPI/Models/CarcelDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CarcelAPI.Models
+{
+    public class CarcelDBContext:DbContext
+    {
+        public DbSet<Preso> Presos { get; set; }
+
+        public DbSet<Delito> Delitos { get; set; }
+
+        public DbSet<Juez> Jueces { get; set; }
+
+        public DbSet<Condena> Condenas { get; set; }
+
+        public DbSet<CondenaDelito> CondenaDelitos { get; set; }
+    }
+}
